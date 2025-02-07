37da7de5e1cf40d9a4f8f0c66eae8e5ccd135d75
Ovie Revaldi <ovierevaldi@gmail.com>
2025-02-07 10:39:14 +0700

    Add navigation to food builder in Title component: enable users to navigate to the food builder page by clicking on the ingredients link

--- src/app/Components/Title.tsx
+++ src/app/Components/Title.tsx
@@ -6,14 +6,19 @@ import React from 'react'
 const Title: React.FC = () => {
 
   const router = useRouter();
+  
   const backToHomePage = () => {
     router.push('/')
   };
+
+  const goToFoodBuilder = () => {
+    router.push('/food_builder');
+  }
   
   return (
     <>
       <h1 className="text-5xl text-center mb-8 cursor-pointer" onClick={backToHomePage}>Recipe Finder</h1>
-      <p className="text-2xl text-center">Search the ingredients for food you desire! <br /> Or build food by <span className='underline italic cursor-pointer'>ingredients you have</span></p>
+      <p className="text-2xl text-center">Search the ingredients for food you desire! <br /> Or build food by <span className='underline italic cursor-pointer' onClick={goToFoodBuilder}>ingredients you have</span></p>
     </>
   )
 }
