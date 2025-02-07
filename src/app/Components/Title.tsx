022b41e9d95c41c40918228b24b6327d0d25a65c
Ovie Revaldi <ovierevaldi@gmail.com>
2025-02-07 10:28:05 +0700

    Update Title component description: enhance user guidance by adding information on building food with available ingredients

--- src/app/Components/Title.tsx
+++ src/app/Components/Title.tsx
@@ -13,7 +13,7 @@ const Title: React.FC = () => {
   return (
     <>
       <h1 className="text-5xl text-center mb-8 cursor-pointer" onClick={backToHomePage}>Recipe Finder</h1>
-      <p className="text-2xl text-center">Search the ingredients for food you desire!</p>
+      <p className="text-2xl text-center">Search the ingredients for food you desire! <br /> Or build food by <span className='underline italic cursor-pointer'>ingredients you have</span></p>
     </>
   )
 }
